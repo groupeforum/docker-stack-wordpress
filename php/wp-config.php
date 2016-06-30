64321c087938db45baa622d4f3e74f8cf8030e01
Florian Girardey <fgirardey@groupeforum.pro>
2016-06-30 18:32:12 +0200

    Initial commit

--- php/wp-config.php
+++ php/wp-config.php
@@ -1,13 +1,11 @@
 <?php
 
-require_once( dirname( __FILE__ ) . '/wp-config-global.php' );
-
-/* That's all, stop editing! Happy blogging. */
-
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
+
+require_once( ABSPATH . 'wp-config-global.php' );
 
 /** Sets up WordPress vars and included files. */
 require_once( ABSPATH . 'wp-settings.php' );
